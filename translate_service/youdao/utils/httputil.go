260f3ee6b88d006fbdee103f75d7281df8f41287
byzze <byzze@qq.com>
2025-11-19 16:31:56 +0800

    refactor(http): optimize client usage with connection pooling and context timeout

--- translate_service/youdao/utils/httputil.go
+++ translate_service/youdao/utils/httputil.go
@@ -1,18 +1,22 @@
 package utils
 
 import (
+	"context"
 	"io"
 	"log/slog"
 	"net/http"
 	neturl "net/url"
 	"strings"
 	"time"
+
+	"handy-translate/utils/httpclient"
 )
 
 func DoGet(url string, header map[string][]string, paramsMap map[string][]string, expectContentType string) []byte {
-	client := &http.Client{
-		Timeout: time.Second * 3,
-	}
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	client := httpclient.GetDefaultClient()
 	params := neturl.Values{}
 	for k, v := range paramsMap {
 		params[k] = v
@@ -20,7 +24,7 @@ func DoGet(url string, header map[string][]string, paramsMap map[string][]string
 	parseUrl, _ := neturl.Parse(url)
 	parseUrl.RawQuery = params.Encode()
 
-	req, _ := http.NewRequest("GET", parseUrl.String(), nil)
+	req, _ := http.NewRequestWithContext(ctx, "GET", parseUrl.String(), nil)
 	for k, v := range header {
 		for hv := range v {
 			req.Header.Add(k, v[hv])
@@ -42,16 +46,17 @@ func DoGet(url string, header map[string][]string, paramsMap map[string][]string
 }
 
 func DoPost(url string, header map[string][]string, bodyMap map[string][]string, expectContentType string) []byte {
-	client := &http.Client{
-		Timeout: time.Second * 3,
-	}
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	client := httpclient.GetDefaultClient()
 	params := neturl.Values{}
 	for k, v := range bodyMap {
 		for pv := range v {
 			params.Add(k, v[pv])
 		}
 	}
-	req, _ := http.NewRequest("POST", url, strings.NewReader(params.Encode()))
+	req, _ := http.NewRequestWithContext(ctx, "POST", url, strings.NewReader(params.Encode()))
 	for k, v := range header {
 		for hv := range v {
 			req.Header.Add(k, v[hv])
